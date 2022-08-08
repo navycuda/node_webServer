0468cf67446599e4f18b880f512fc618f671471f
Shane <navysupra@gmail.com>
2022-08-08 03:19:48 +0000

    Successfully had the node webserver running

--- httpServer.js
+++ httpServer.js
@@ -1,9 +1,16 @@
 /* Require */
-const request = require(`request`);
+const http  = require(`http`);
 
 /* Tcp:Http */
 /* Arguments & Properties */
 /* Export Functions */
 /* Local Functions */
 /* Execution & Test Data */
+const server = http.createServer(((request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello World\n');
+
+}));
+
+server.listen(3000);
 /* Exports */
